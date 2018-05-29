61be1cc885cbd9c07837efe25a3bc1728a5db631
pttharun <pttharun@gmail.com>
2018-05-29 13:55:03 +0530

    Add files via upload

---
+++ Magento/custom/web/js/custom.js
@@ -0,0 +1,13 @@
+requirejs(['jquery',
+    'owl.carousel/owl.carousel.min',
+    'mage/apply/main',
+    "jquery/jquery.cookie",
+    "kinetic"
+], function ($) {
+    
+    
+    jQuery(document).ready(function ($) {
+        
+    });
+    
+});
